1398aa8c2561415557ef2af2d15f142945159819
tuanlt79 <tuanlt79@outlook.com.vn>
2021-06-18 16:32:50 +0700

    app_scroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss";
 import Home from "./Pages/Home/Home";
-import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
+import { withRouter, Route, Router, Switch } from "react-router-dom";
 import Loading from "./Component/Loading/Loading.jsx";
 import Detail from "./Component/Detail/Detail";
 import { HeaderTemplate } from "./template/HeaderTemplate";
@@ -12,25 +12,32 @@ import Profile from "./Component/Profile/Profile";
 import { Provider } from "react-redux";
 import AdminB from "./Component/Admin/AdminB";
 import { store } from "./Redux/configStore";
-import EditUser from "./Component/Admin/EditUser";
+import { useLayoutEffect } from "react";
 export const history = createBrowserHistory();
+const ScrollToTop = withRouter(({ children, location: { pathname } }) => {
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return children || null;
+});
 function App() {
   return (
     <Router history={history}>
       <Provider store={store}>
         <div className="App">
           <Loading />
-          <Switch>
-            <Route exact path="/home" component={Home} />
-            <HeaderTemplate exact path="/detail/:id" Component={Detail} />
-            <Route exact path="/checkout/:id" component={Checkout} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <HeaderTemplate exact path="/profile" Component={Profile} />
-            <Route exact path="/admin" component={AdminB} />
-            {/* <Route exact path="/admin/edituser/:id" component={EditUser} /> */}
-            <Route exact path="/" component={Home} />
-          </Switch>
+          <ScrollToTop>
+            <Switch>
+              <Route exact path="/home" component={Home} />
+              <HeaderTemplate exact path="/detail/:id" Component={Detail} />
+              <Route exact path="/checkout/:id" component={Checkout} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <HeaderTemplate exact path="/profile" Component={Profile} />
+              <Route exact path="/admin" component={AdminB} />
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </ScrollToTop>
         </div>
       </Provider>
     </Router>
